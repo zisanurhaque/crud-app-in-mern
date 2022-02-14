f1bd192daec4ed34db4a6de5d8bd8d426fc0fe1f
Zisanur Haque <mdzisanulhaque2000@gmail.com>
2022-02-14 20:18:25 +0600

    14-2-2022 2nd

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ if(process.env.NODE_ENV === 'production'){
     app.get("*", (req, res) => {
         res.sendFile(path.resolved(__dirname, 'client', 'build', 'index.html'))
     })
-}
+} 
 
 app.listen(Port, () => {
     console.log(`Server Starting At Port ${Port}`)
