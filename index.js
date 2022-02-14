6035d55a55dc166c54c88f688d576d1d0010d0eb
Zisanur Haque <mdzisanulhaque2000@gmail.com>
2022-02-14 23:01:58 +0600

    14-2-2022 6th

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv')
 const app = express();
+const router = express.Router()
 const path = require('path')
 
 dotenv.config({path: './config.env'})
@@ -24,12 +25,12 @@ app.use(cors());
 
 const UserSchema = require('./Schema');
 
-app.get("/get", async(req, res) => {
+router.get("/get", async(req, res) => {
     const students = await UserSchema.find()
     res.json({students})
 });
 
-app.post("/add", async(req, res) => {
+router.post("/add", async(req, res) => {
     const {name, email, dept} = req.body
 
     try{
@@ -45,12 +46,12 @@ app.post("/add", async(req, res) => {
     }
 })
 
-app.delete("/studetns/:id", async(req,res) => {
+router.delete("/studetns/:id", async(req,res) => {
     const del = await UserSchema.findByIdAndDelete(req.params.id);
     res.json({del})
 })
 
-app.put("/update/:id", async(req, res) => {
+router.put("/update/:id", async(req, res) => {
     const {name, email, dept} = req.body
     const upd = await UserSchema.findByIdAndUpdate(req.params.id, {
         name: name,
@@ -60,7 +61,7 @@ app.put("/update/:id", async(req, res) => {
     res.json({upd})
 })
 
-app.get("/single/:id", async(req,res) => {
+router.get("/single/:id", async(req,res) => {
     const single = await UserSchema.findById(req.params.id)
     res.json({single})
 })
